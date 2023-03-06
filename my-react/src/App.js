250c37b2f281dd923dd0519962a9017fce36a74a
-dav <davtyan.arman1996@gmail.com>
2023-03-06 13:14:02 +0400

    last changes

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -2,7 +2,8 @@ import logo from "./logo.svg";
 import One from "./Components/One"
 import Clock from "./Components/Clock";
 import Test from "./Components/Test"
-// import FormattedDate from "./Components/newClock";
+
+
 
 function GetTime(props) { 
   return (
@@ -13,30 +14,23 @@ function GetTime(props) {
 }
 
 
-function App(){
+function App() {
   return (
     <div>
-      
-{/*     
-      <FormattedDate date={this.state.date} />
-      <div> </div> */}
-      <input type="text" className="input"/>
-      <input type="password" className="input" />
+    
       <input type="range" className="input" />
       <input type="date" className="input" />
       <input type="checkbox" className="input" />
       <input type="file" className="input" />
-      <input type="color" className="input" 
-        
-      />
+      <input type="color" className="input" />
+    
       <Clock />
       <GetTime name={"Arman"} lastName={"Davtyan"} />
       <Test v={"string"} b={122 % 2 === 0 ? 10 : 20} c={true} d={false} e={undefined} f={null} g={[1, 2, 3]} />
      
+      
     </div>
   )
-
-
 }
 
-export default App 
+export default App   
