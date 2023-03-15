144f70255b4bde37dddd8c095cff7fe87d068a8a
-dav <davtyan.arman1996@gmail.com>
2023-03-15 23:01:17 +0400

    no changes

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -9,20 +9,11 @@ import Breeds from "./Components/Breeds";
 
 function App() {
   return (
-    <div>
-    
-    //   <input type="range" className="input" />
-    //   <input type="date" className="input" />
-    //   <input type="checkbox" className="input" />
-    //   <input type="file" className="input" />
-    //   <input type="color" className="input" />
-    
-    //   <Clock />
-    //   <Test v={"string"} b={122 % 2 === 0 ? 10 : 20} c={true} d={false} e={undefined} f={null} g={[1, 2, 3]} />
-    //   <Calc />
+  <div> 
+    <Clock />
       <Breeds />
       
-    </div>
+    </div >
   )
 }
 
--- my-react/src/Components/Breeds.js
+++ my-react/src/Components/Breeds.js
@@ -1,24 +1,44 @@
 import React from "react";
+import { v4 } from "uuid"
 
 export default class Breeds extends React.Component {
     state = {
-        breeds: []
+        breeds: [],
+        src: ""
     }
 
+    onBreedClick(breed) {
+
+        fetch(`https://dog.ceo/api/breed/${breed}/images`)
+            .then(responce => responce.json())
+            .then(result => this.setState({
+                src: result.message[0]
+            }))
+
+
+    }
 
     componentDidMount() {
         fetch("https://dog.ceo/api/breeds/list/all ")
             .then(responce => responce.json())
             .then(result => this.setState({
-                breeds: Object.keys(result.message)
+                breeds: Object.keys(result.message),
+
+
+
             }))
 
     }
 
     render() {
 
+
         return (
-            <div> {this.state.breeds.map(breed => <div> {breed} </div>)} </div>
+            <>
+                <img src={this.state.src} />
+                <div> {this.state.breeds.map(breed => <div onClick={(evt) => this.onBreedClick(breed)}> {breed} </div>)} </div>
+            </>
+
 
         )
     }
